cd79d443e86d8c9cfd42f47ca5b9ab6d367e7e6a
Griffin Meyer <griffinmeyer@Griffins-MacBook-Pro.local>
2016-03-05 15:14:30 -0800

    Got basic Spotify song playing API working

--- app/src/main/java/com/example/glmeyer/synchronizedforspotify/MainActivity.java
+++ app/src/main/java/com/example/glmeyer/synchronizedforspotify/MainActivity.java
@@ -1,13 +1,154 @@
 package com.example.glmeyer.synchronizedforspotify;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements
+        PlayerNotificationCallback, ConnectionStateCallback {
+    private final static int REQUEST_ENABLE_BT = 1;
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "90eb86d7dd924772994f5134d9eb6cab";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "http://griffinmeyer.com/callback/";
 
+    private Player mPlayer;
+    // Request code that will be used to verify if the result comes from correct activity
+// Can be any integer
+    private static final int REQUEST_CODE = 1337;
+    BluetoothAdapter mBluetooth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+        mBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetooth == null){
+            //No bluetooth support
+        }
+        if(!mBluetooth.isEnabled()){
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new Player.InitializationObserver() {
+                    @Override
+                    public void onInitialized(Player player) {
+                        mPlayer = player;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addPlayerNotificationCallback(MainActivity.this);
+
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
+    }
+
+    public void logOut(View v){
+        AuthenticationClient.clearCookies(this);
+    }
+
+    public void playSong(View v){
+        //EditText et = (EditText)findViewById(R.id.editText);
+        mPlayer.play("spotify:track:6NsRMcD9MBp9sYXtcnJSGK");
+    }
+    boolean paused = false;
+    public void pauseSong(View v){
+        if (paused == true){
+            mPlayer.resume();
+            paused = false;
+        }else if(paused == false) {
+            mPlayer.pause();
+            paused = true;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("MainActivity", "Playback event received: " + eventType.name());
+        switch (eventType) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("MainActivity", "Playback error received: " + errorType.name());
+        switch (errorType) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
     }
 }
